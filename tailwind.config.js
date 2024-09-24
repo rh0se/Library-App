2c0a15614eb9ab6560cb25762080015476f4e376
rh0se <momirhoda@gmail.com>
2024-09-24 16:27:34 +0100

    updating tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { 
+        "Montserrat": ["Montserrat", "sans-serif"] 
+    } 
+    },
   },
   plugins: [],
 }
