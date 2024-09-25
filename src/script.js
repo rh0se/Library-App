af56032f809a47263914857e9dd7cc72cf75b1cb
rh0se <momirhoda@gmail.com>
2024-09-25 22:44:45 +0100

    updating html

--- src/script.js
+++ src/script.js
@@ -26,7 +26,7 @@ function Book() {
     for (let i = 0; i < bookData.length; i++) {
         if (bookData[i]) {
             library.innerHTML += `<article class="bg-white w-64 font-bold rounded-lg p-6 flex gap-1.5 flex-col"><div class="flex justify-center"><p>Title:</p><p class="ms-1">${bookData[i].title}</p></div><div class="flex justify-center"><p>Author: </p><p class="ms-1">${bookData[i].author}</p></div><div class="flex justify-center"><p>Pages:</p><p class="ms-1"> ${bookData[i].pages}</p></div>
-        <div class="flex justify-center"><button onclick=changeRead(${i}) class="focus:outline-none text-white ${bookData[i].color} hover:border-2 hover:border-black font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" id="read-button">${bookData[i].read}</button><button class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2" onclick=deleteBook(${i})><i class="fa-solid fa-trash-can" style="color: text-white;"></i></button><div>
+        <div class="flex justify-center"><button aria-label="read or not read" onclick=changeRead(${i}) class="focus:outline-none text-white ${bookData[i].color} hover:border-2 hover:border-black font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2" id="read-button">${bookData[i].read}</button><button aria-label="Delete Book" class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2" onclick=deleteBook(${i})><i class="fa-solid fa-trash-can" style="color: text-white;"></i></button><div>
          </article>`;
         }
         
